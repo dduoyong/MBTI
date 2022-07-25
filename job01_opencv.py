e8c05b76d0067bae1c2cde6bf572120a0e8527a4
Github <dayoung.biz@gmail.com>
2022-07-25 17:54:01 +0900

    job01_opencv_success

--- job01_opencv.py
+++ job01_opencv.py
@@ -2,8 +2,10 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import model_from_json
+import matplotlib.pyplot as plt
 import pandas as pd
 from PIL import Image
+import time
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -26,11 +28,35 @@ model.load_weights("./models/22emo_1st_model_accuracy_81.h5")
 # emotion_model = model_from_json(json)
 # emotion_model.load_weights('./models/22emo_1st_model_accuracy_81.h5')
 
-global cap
+# global cap
 cap = cv2.VideoCapture(0)
-class VideoCamera(object):
-  def get_frame(self):
-    # global df
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 650)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 500)
+# class VideoCamera(object):
+#   def get_frame(self):
+#     # global df
+#     ret, frame = cap.read()
+#     frame = cv2.resize(frame, (650, 500))
+#
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces = face_detector.detectMultiScale(gray, 1.3, 5, minSize=(30, 30))
+#     # df = pd.read_csv(music_dict[emt[0]])
+#     # df = df.head(12)
+#     for (x, y, w, h) in faces:
+#       roi_gray = gray[y:y + h, x:x + w]
+#       input = np.expand_dims(np.expand_dims(cv2.resize(frame, (64, 64, 3)), -1), 0)
+#       prediction = model.predict(input)
+#       idx = np.argmax(prediction)
+#       emt[0] = idx
+#       # df = get_music()
+#       cv2.rectangle(frame, (x, y), (x + w, y + h), (124, 252, 0), 2)
+#       cv2.putText(frame, emotion_dict[idx], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+#     last_frame = frame.copy()
+#     jpeg = cv2.imencode('.jpg', last_frame)[1].tobytes()
+#     return jpeg
+
+flag = True
+while flag:
     ret, frame = cap.read()
     frame = cv2.resize(frame, (650, 500))
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -39,7 +65,7 @@ class VideoCamera(object):
     # df = df.head(12)
     for (x, y, w, h) in faces:
       roi_gray = gray[y:y + h, x:x + w]
-      input = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+      input = np.expand_dims(np.expand_dims(cv2.resize(frame, (64, 64)), -1), 0)
       prediction = model.predict(input)
       idx = np.argmax(prediction)
       emt[0] = idx
@@ -47,5 +73,32 @@ class VideoCamera(object):
       cv2.rectangle(frame, (x, y), (x + w, y + h), (124, 252, 0), 2)
       cv2.putText(frame, emotion_dict[idx], (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
     last_frame = frame.copy()
-    jpeg = cv2.imencode('.jpg', last_frame)[1].tobytes()
-    return jpeg
+
+    cv2.imshow('VideoFrame', frame)
+    time.sleep(1 / 30)
+    key = cv2.waitKey(33)
+    if key == 27:
+        flag = False
+cap.release()
+
+# capture = cv2.VideoCapture(0)
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# capture.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+# flag = True
+# while flag:
+#     _, frame = capture.read()
+#     cv2.imshow('VideoFrame', frame)
+#     #카메라로부터 캡쳐된 이미지를 아래와 같이 저장한다
+#     cv2.imwrite('./capture.png', frame)
+#
+#     time.sleep(1/2)
+#     #보통 1초에 30장은 되야 자연스러움 1/30
+#     #초당 2번 찍겠다는 의미
+#
+#     key = cv2.waitKey(33)
+#     if key == 27:
+#         flag = False
+#
+#
+#
+
