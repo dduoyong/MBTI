ee8a5ffa83756fe5b5c0450966048c48bb9d00e0
Github <dayoung.biz@gmail.com>
2022-08-03 14:32:58 +0900

    melon_clear_kor_lyric_02

--- melon_job04_k_lyric_preprocessing.py
+++ melon_job04_k_lyric_preprocessing.py
@@ -11,11 +11,13 @@ for i in range(4, 5):
     df = pd.read_csv('./melon/03_melon_lyric_concat_data/{}_lyric_concat.csv'.format(gn))
     df.info()
 
-    df_stopwords = pd.read_csv('./melon/kor_stopwords.csv')
+    df_stopwords = pd.read_csv('./melon/melon_lyrics_kor_stopwords.csv')
     df_stopwords.info()
 
     stopwords = list(df_stopwords['stopwords'])
-    stopwords = stopwords + ['라라라라', '라라']
+    # stopwords = stopwords + ['띠띠띠띠띠띠띠', '뚜루뚜루 ','뚜뚜뚜', '나나', '두루', '루루', '음음음', '음음', '꼬르르르륵',
+    #                          '삐릿삐릿삐릿', '랄랄랄랄', '랄랄라', '라라', '스르르', '르르', '삼백육십오일', '우우', '난나',
+    #                          '라라라라', '라라']
 
     okt = Okt()
     Korean_lyric = []
@@ -37,9 +39,9 @@ for i in range(4, 5):
         cleaned_sentence = ' '.join(words)
         Korean_lyric.append(cleaned_sentence)
 
-df['Korean_clean_lyric'] = Korean_lyric
-df = df[['artist', 'title', 'Korean_clean_lyric']]
-df.dropna(inplace=True)
+    df['Korean_clean_lyric'] = Korean_lyric
+    df = df[['artist', 'title', 'Korean_clean_lyric']]
+    df.dropna(inplace=True)
 
-df.to_csv('./melon/04_melon_clear_lyric/{}_clean_kor_lyric.csv'.format(gn), index=False)
-df.info()
+    df.to_csv('./melon/04_melon_clear_lyric/{}_clean_kor_lyric_02.csv'.format(gn), index=False)
+    df.info()
