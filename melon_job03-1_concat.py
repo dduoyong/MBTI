c70e15e80474d685affbb7cf5a46aa8e02406f1c
hyunk <0620julie@gmail.com>
2022-08-03 11:31:17 +0900

    melon lyric concat final code

--- melon_job03-1_concat.py
+++ melon_job03-1_concat.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import glob
 
-
-music_genre_lst = ['Adultpop', 'Ballad', 'Classic', 'Dance', 'FandB', 'Idol', 'Indie', 'Jazz', 'Jpop', 'NewAge', 'Pop', 'RandB_S', 'RandH', 'RandM']
+# ---- 크롤링 된 가사 데이터 확인 후 최종 10개 장르 선정 ----
+music_genre_lst = ['Adultpop', 'Ballad', 'Dance', 'FandB', 'Idol', 'Indie', 'Pop', 'RandB_S', 'RandH', 'RandM']
 
 for i in music_genre_lst:
-
-    data_paths = glob.glob('melon_lyrics_crawling_data/{}/*'.format(i))
+    data_paths = glob.glob('./melon/02_melon_lyric_crawling_data/{}/*'.format(i))
     df = pd.DataFrame()
-    df.info()
+    # df.info()
 
     for path in data_paths:
         df_temp = pd.read_csv(path)
@@ -16,16 +15,7 @@ for i in music_genre_lst:
         df_temp.drop_duplicates(inplace=True)
         df = pd.concat([df, df_temp], ignore_index=True)
 
-
-# for i in range(1, 38):
-#     df_temp = pd.read_csv('./crawling_data/reviews_2020_{}page.csv'.format(i))
-#     df_temp.dropna(inplace=True)
-#     df_temp.drop_duplicates(inplace=True)
-#     df = pd.concat([df, df_temp], ignore_index=True)
-
     df.drop_duplicates(inplace=True)
     df.info()
 
-    df.to_csv('./concat_lyrics_crawling_data/crawling_data.lyrics_{}.csv'.format(i), index = False)
-
-# df.to_csv('./melon_lyric_data/reviews_2017_2022.csv', index = False)
+    df.to_csv('./melon/03_melon_lyric_concat_data/{}_lyric_concat.csv'.format(i), index = False)
