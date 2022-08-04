18b5df1b6d736fe1b78198c2a7a7fe3728ad69d2
hyunk <0620julie@gmail.com>
2022-08-04 10:16:49 +0900

    melon lyric preprocessing code 03

--- melon_job04-2_e_lyric_preprocessing.py
+++ melon_job04-2_e_lyric_preprocessing.py
@@ -24,13 +24,14 @@ for i in range(6, 7):
         lyrics = re.sub('[^a-zA-Z]', ' ', lyrics)
         #  -- 모든 영어 텍스트 소문자로 대체 --
         small_lyrics = lyrics.lower()
+        print('small_lyrics :', small_lyrics)
 
         # -- 토큰화 --
         # nltk.download('tagsets')
         # nltk.download('averaged_perceptron_tagger')
         # nltk.download('wordnet')
         token_words = word_tokenize(small_lyrics)
-        # print(token_words)
+        print('token_words :', token_words)
         # exit()
 
         # lemm = WordNetLemmatizer()
@@ -55,42 +56,49 @@ for i in range(6, 7):
         # exit()
 
         lemmatizer = WordNetLemmatizer()
-        df_verb = df_token[((df_token['class'] == 'VB') | (df_token['class'] == 'VBD') | (df_token['class'] == 'VBG') | (df_token['class'] == 'VBN'))]
-        df_verb.info()
-        print('df_verb :', df_verb)
+        df_noun = df_token[(df_token['class'] == 'NN') | (df_token['class'] == 'NNS')]
+        df_verb = df_token[(df_token['class'] == 'VB') | (df_token['class'] == 'VBD') | (df_token['class'] == 'VBG') | (df_token['class'] == 'VBN')]
+        df_adjective = df_token[(df_token['class'] == 'JJ') | (df_token['class'] == 'JJR') | (df_token['class'] == 'JJS')]
+        df_adverb = df_token[(df_token['class'] == 'RB') | (df_token['class'] == 'RBR') | (df_token['class'] == 'RBS')]
+        # df_verb.info()
+        # print(df_verb)
         # print('표제어 추출 전 :', token_words)
-        # print('표제어 추출 후 :', [lemmatizer.lemmatize(word) for word in df_token['word']])
+        # print('표제어 추출 후 :', [lemmatizer.lemmatize(word) for word in df_verb['word']])
 
-        print([lemmatizer.lemmatize(w, pos='v') for w in df_verb['word']])
+        print([lemmatizer.lemmatize(w, pos='n') for w in df_noun['word']])   #명사
+        print([lemmatizer.lemmatize(w, pos='v') for w in df_verb['word']])    #동사
+        print([lemmatizer.lemmatize(w, pos='a') for w in df_adjective['word']])   #형용사
+        print([lemmatizer.lemmatize(w, pos='r') for w in df_adverb['word']])   #부사
         # exit()
 
 
-    #     # -- nltk stopwords & 불용어 제거 --
-    #     stop_words = set(stopwords.words('english'))
+        # -- nltk stopwords & 불용어 제거 --
+        stop_words = set(stopwords.words('english'))
+
+
+        result = []
+        for token in token_words:
+            if token not in stop_words:
+                result.append(token)
+
+        # print(token_words)
+        # print(result)
+
+        lemm = WordNetLemmatizer()
+        # for word in token_words:
+        # print([lemm.lemmatize(word, pos='v') for word in token_words])
+
+        # exit()
+
+
+        cleaned_sentence = ' '.join(result)
+        English_lyric.append(cleaned_sentence)
+        print(English_lyric)
+
+    # df['English_clean_lyric'] = English_lyric
+    # df = df[['artist', 'title', 'English_clean_lyric']]
+    # df.dropna(inplace=True)
     #
-    #
-    #     result = []
-    #     for token in token_words:
-    #         if token not in stop_words:
-    #             result.append(token)
-    #
-    #     # print(token_words)
-    #     # print(result)
-    #
-    #     lemm = WordNetLemmatizer()
-    #     # for word in token_words:
-    #     # print([lemm.lemmatize(word, pos='v') for word in token_words])
-    #
-    #     # exit()
-    #
-    #
-    #     cleaned_sentence = ' '.join(result)
-    #     English_lyric.append(cleaned_sentence)
-    #
-    # # df['English_clean_lyric'] = English_lyric
-    # # df = df[['artist', 'title', 'English_clean_lyric']]
-    # # df.dropna(inplace=True)
-    # #
-    # # df.to_csv('./melon/04_melon_clear_lyric/{}_clean_eng_lyric_delete_고유명사.csv'.format(gn), index=False)
-    # # df.info()
+    # df.to_csv('./melon/04_melon_clear_lyric/{}_clean_eng_lyric_delete_고유명사.csv'.format(gn), index=False)
+    # df.info()
 
